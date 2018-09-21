12e222adb4d841abab03e0bf2d59d6f44d52e0c4
eugedelatorre <eugedelatorre@hotmail.com>
2018-09-21 10:41:34 -0300

    creación de header + links

---
+++ faq.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Felicitrip - FAQ</title>
+
+  </head>
+  <body>
+    <ul>
+      <li><a href="#"></a>Login</li>
+      <li><a href="#"></a>Registrarme</li>
+    </ul>
+
+  </body>
+</html>
---
+++ registro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Felicitrip - Registro</title>
+  </head>
+  <body>
+
+    <img src="./images/logo.png" alt="logo">
+
+    <ul>
+      <li><a href="#"></a>Login</li>
+      <li><a href=registro.php>Registrarme</a></li>
+      <li><a href=faq.php>FAQ</a></li>
+    </ul>
+
+  </body>
+</html>
