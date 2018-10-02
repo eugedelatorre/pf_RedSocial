9dc249460669292eeebba38cbcb3a6803f0ebe00
nahuelns <42101881+nahuelns@users.noreply.github.com>
2018-10-02 17:19:17 -0300

    Perfil + CssPerfil

---
+++ perfil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Bad+Script|Roboto" rel="stylesheet">
+  </head>
+
+  <body>
+    <section class="contenedorPrincipal">
+      <img class="imagenPerfil"  src="images/fondo_perfil.jpg" alt="imagen ciudad">
+      <nav class="navPerfil">
+        <ul class="listPerfilL" >
+          <li><a>Presentacion</a></li>
+          <li><a>Fotos</a></li>
+          <li><a>Video</a></li>
+        </ul>
+        <ul class="listPerfilR" >
+          <li><a>Post</a></li>
+          <li><a>Seguidores</a></li>
+          <li><a>Recomendaciones</a></li>
+        </ul>
+
+      </nav>
+        <img class="avatar"  src="images/imagen_perfil.jpg">
+    </section>
+    <section class="contenedorSecundario">
+      <nav class="quiensoy">
+        <h3>QUIEN SOY?</h3>
+        <br>
+        <a >Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <br>
+          <br><br>
+          <nav class="datosCuenta">
+            <h3>Cuenta</h3>
+            <br>
+            <a >Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              quis nostrud exercitation ullamco laboris nisi
+            </a>
+          </nav>
+
+        </a>
+      </nav>
+
+    </section>
+  <body>
+    <div>
+
+    </div>
+
+  </body>
